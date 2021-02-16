381a4542335221e3d5a69920ca7a580e5d798897
Mike <mikef907@gmail.com>
2021-02-15 22:32:23 -0900

    implemented basic login

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,9 +4,15 @@ import { AppProps } from 'next/app';
 import { ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import theme from '../themes';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 
 export default function MyApp(props: AppProps) {
   const { Component, pageProps } = props;
+
+  const client = new ApolloClient({
+    uri: 'http://localhost:4000/graphql',
+    cache: new InMemoryCache(),
+  });
 
   React.useEffect(() => {
     // Remove the server-side injected CSS.
@@ -19,16 +25,19 @@ export default function MyApp(props: AppProps) {
   return (
     <React.Fragment>
       <Head>
-        <title>My page</title>
+        <title>PIPA</title>
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width"
         />
       </Head>
+
       <ThemeProvider theme={theme}>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
-        <Component {...pageProps} />
+        <ApolloProvider client={client}>
+          <Component {...pageProps} />
+        </ApolloProvider>
       </ThemeProvider>
     </React.Fragment>
   );
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,5 +1,4 @@
 import {
-  Box,
   FormControl,
   Grid,
   Input,
@@ -8,10 +7,128 @@ import {
   Button,
   Link,
 } from '@material-ui/core';
-import { Container } from 'next/app';
 import Layout from '../components/Layout';
+import { gql, useLazyQuery, useQuery } from '@apollo/client';
+import React, { useState } from 'react';
+
+interface ILogin {
+  email: string;
+  password: string;
+}
+
+const LOGIN = gql`
+  query Login($email: String!, $password: String!) {
+    login(email: $email, password: $password)
+  }
+`;
+
+// export default class LoginForm extends React.Component {
+//   constructor(props: any) {
+//     super(props);
+//     this.state = { email: '', password: '' };
+//     this.handleEmailChange = this.handleEmailChange.bind(this);
+//     this.handlePasswordChange = this.handlePasswordChange.bind(this);
+//     this.handleSubmit = this.handleSubmit.bind(this);
+//   }
+
+//   handleEmailChange(event: any) {
+//     console.log(event.target.value);
+//     this.setState({ email: event.target.value });
+//   }
+
+//   handlePasswordChange(event: any) {
+//     console.log(event.target.value);
+//     this.setState({ password: event.target.value });
+//   }
+
+//   handleSubmit(event: any) {
+//     console.log('email', this.state.email);
+//     console.log('password', this.state.password);
+
+//     let email = this.state.email;
+//     let password = this.state.password;
+
+//     useQuery(LOGIN, { variables: { email, password } });
+//     event.preventDefault();
+//   }
+
+//   render() {
+//     return (
+//       <Layout>
+//         <Typography
+//           variant="h4"
+//           component="h1"
+//           style={{ textAlign: 'center' }}
+//           gutterBottom
+//         >
+//           Login
+//         </Typography>
+//         <Grid
+//           container
+//           alignContent="center"
+//           justify="center"
+//           direction="column"
+//           spacing={2}
+//         >
+//           <Grid item xs={12}>
+//             <Grid spacing={1} container direction="row">
+//               <Grid item xs={12} md={6}>
+//                 <FormControl fullWidth>
+//                   <InputLabel htmlFor="email">Email Address</InputLabel>
+//                   <Input
+//                     id="email"
+//                     type="email"
+//                     value={this.state.email}
+//                     onChange={this.handleEmailChange}
+//                   ></Input>
+//                 </FormControl>
+//               </Grid>
+//               <Grid item xs={12} md={6}>
+//                 <FormControl fullWidth>
+//                   <InputLabel htmlFor="password">Password</InputLabel>
+//                   <Input
+//                     id="password"
+//                     type="password"
+//                     value={this.state.password}
+//                     onChange={this.handlePasswordChange}
+//                   ></Input>
+//                 </FormControl>
+//               </Grid>
+//               <Grid item md={4}>
+//                 <Button
+//                   style={{ alignSelf: 'flex-end' }}
+//                   variant="contained"
+//                   onClick={this.handleSubmit}
+//                 >
+//                   Login
+//                 </Button>
+//               </Grid>
+//             </Grid>
+//           </Grid>
+//           <Grid item>
+//             <Grid spacing={4} container>
+//               <Grid item>
+//                 <Link>Forgot Password?</Link>
+//               </Grid>
+//               <Grid item>
+//                 <Link>Create Account</Link>
+//               </Grid>
+//             </Grid>
+//           </Grid>
+//         </Grid>
+//       </Layout>
+//     );
+//   }
+// }
 
 export default function Login() {
+  const [formState, setFormState] = useState({
+    email: '',
+    password: '',
+  });
+
+  const [login, { data }]: any = useLazyQuery(LOGIN);
+
   return (
     <Layout>
       <Typography
@@ -34,17 +151,43 @@ export default function Login() {
             <Grid item xs={12} md={6}>
               <FormControl fullWidth>
                 <InputLabel htmlFor="email">Email Address</InputLabel>
-                <Input id="email" type="email"></Input>
+                <Input
+                  id="email"
+                  type="email"
+                  value={formState.email}
+                  onChange={(e) =>
+                    setFormState({ ...formState, email: e.target.value })
+                  }
+                ></Input>
               </FormControl>
             </Grid>
             <Grid item xs={12} md={6}>
               <FormControl fullWidth>
                 <InputLabel htmlFor="password">Password</InputLabel>
-                <Input id="password" type="password"></Input>
+                <Input
+                  id="password"
+                  type="password"
+                  value={formState.password}
+                  onChange={(e) =>
+                    setFormState({ ...formState, password: e.target.value })
+                  }
+                ></Input>
               </FormControl>
             </Grid>
             <Grid item md={4}>
-              <Button style={{ alignSelf: 'flex-end' }} variant="contained">
+              <Button
+                style={{ alignSelf: 'flex-end' }}
+                variant="contained"
+                onClick={(e) => {
+                  e.preventDefault();
+                  login({
+                    variables: {
+                      email: formState.email,
+                      password: formState.password,
+                    },
+                  });
+                }}
+              >
                 Login
               </Button>
             </Grid>
@@ -61,6 +204,7 @@ export default function Login() {
           </Grid>
         </Grid>
       </Grid>
+      {data?.login}
     </Layout>
   );
 }
