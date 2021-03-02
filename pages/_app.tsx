b1ed69673ca1ecc2104ccd2364e24953dcb12fdf
Mike <mikef907@gmail.com>
2021-03-01 22:51:43 -0900

    wip custom scalar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,27 +5,159 @@ import { ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import theme from '../themes';
 import '../styles/globals.css';
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import {
+  ApolloClient,
+  ApolloLink,
+  ApolloProvider,
+  gql,
+  HttpLink,
+  InMemoryCache,
+} from '@apollo/client';
 import {
   IUser,
   setUserFromLocalStorage,
   checkRoles,
   UserContext,
 } from '../context/UserContext';
-import { getStoreKeyName } from '@apollo/client/utilities';
+import { makeExecutableSchema } from '@graphql-tools/schema';
+import { GraphQLScalarType, Kind } from 'graphql';
+import { fetch } from 'cross-fetch';
+import { print, graphqlSync } from 'graphql';
+import { introspectSchema, wrapSchema } from '@graphql-tools/wrap';
+import { withScalars } from 'apollo-link-scalars';
 
-export default function MyApp(props: AppProps) {
+// const executor = async ({ document, variables }) => {
+//   const query = print(document);
+//   const fetchResult = await fetch('http://localhost:4000/graphql', {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify({ query, variables }),
+//   });
+//   return fetchResult.json();
+// };
+
+// const getSchema = async () => {
+//   const schema = wrapSchema({
+//     schema: await introspectSchema(executor),
+//     executor,
+//   });
+//   return schema;
+// };
+
+const MyApp = (props: AppProps) => {
   const { Component, pageProps } = props;
 
   const IS_SERVER = typeof window === 'undefined';
 
-  const setToken = () => {
-    if (!IS_SERVER) return `Bearer ${localStorage.getItem('token')}` || '';
+  // const link = new HttpLink({ uri: 'http://localhost:4000/graphql', fetch });
+
+  //getSchema().then((schema) => console.log(schema));
+
+  // const executableSchema = makeRemoteExecutableSchema({
+  //   schema,
+  //   link,
+  // });
+
+  const typeDefs = gql`
+    directive @specifiedBy(url: String!) on SCALAR
+    scalar DateTime
+
+    input EventInput {
+      parentId: Float
+      name: String!
+      description: String!
+      allDay: Boolean
+      start: DateTime!
+      end: DateTime
+    }
+
+    type EventQuery {
+      id: ID!
+      parentId: Float
+      name: String!
+      description: String!
+      allDay: Boolean
+      start: DateTime!
+      end: DateTime
+      created_at: DateTime!
+      updated_at: DateTime!
+    }
+
+    type Mutation {
+      resetPassword(input: UserResetPassword!): Boolean!
+      forgotPassword(email: String!): Boolean!
+      addUser(data: UserInput!): String!
+      login(email: String!, password: String!): String!
+      addEvent(event: EventInput!): EventQuery!
+    }
+
+    type Query {
+      users: [UserQuery!]!
+      user(id: Float!): UserQuery!
+      events: [EventQuery!]!
+      event(id: Float!): EventQuery!
+    }
+
+    type RoleQuery {
+      id: ID!
+      name: String!
+    }
+
+    input UserInput {
+      first_name: String!
+      last_name: String!
+      email: String!
+      password: String!
+    }
+
+    type UserQuery {
+      id: ID!
+      first_name: String!
+      last_name: String!
+      email: String!
+      roles: [RoleQuery!]!
+    }
+
+    input UserResetPassword {
+      nonce: String!
+      password: String!
+    }
+  `;
+
+  const resolvers = {
+    Date: {
+      name: 'Date',
+      description: 'Date custom scalar type',
+      parseValue(value: any) {
+        return new Date(value); // value from the client
+      },
+      serialize(value: any) {
+        return value.getTime(); // value sent to the client
+      },
+      parseLiteral(ast: any) {
+        if (ast.kind === Kind.INT) {
+          return new Date(+ast.value); // ast value is always in string format
+        }
+        return null;
+      },
+    },
   };
+
+  const schema = makeExecutableSchema({
+    typeDefs,
+  });
+
+  const link = ApolloLink.from([
+    withScalars({ schema, resolvers }),
+    new HttpLink({ uri: 'http://example.org/graphql' }),
+  ]);
 
   const client = new ApolloClient({
     uri: 'http://localhost:4000/graphql',
     cache: new InMemoryCache(),
+    link,
     headers: {
       Authorization:
         !IS_SERVER && localStorage.getItem('token')
@@ -69,4 +201,6 @@ export default function MyApp(props: AppProps) {
       </ThemeProvider>
     </React.Fragment>
   );
-}
+};
+
+export default MyApp;
